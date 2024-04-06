2322df40a913cdacc0e6025267ce709494f509fb
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-04-06 07:58:25 +0300

    add simple code signup by oop

---
+++ Classes/Dbh.php
@@ -0,0 +1,24 @@
+<?php
+
+class Dbh
+{
+    private $host = "localhost";
+    private $dbname = "myfirstdb";
+    private $dbusername = "root";
+    private $dbpassword = "";
+
+    protected function connect()
+    {
+        try {
+            $pdo = new PDO("mysql:host=" . $this->host . ";dbname=" .
+                $this->dbname, $this->dbusername, $this->dbpassword);
+            $pdo->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+            return $pdo;
+        } catch (PDOException $e) {
+            die("connection failed: " . $e->getMessage());
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup Page</title>
+</head>
+
+<body>
+
+    <form action="includes/signup.inc.php" method="post">
+        <input type="text" name="username">
+        <input type="password" name="pwd">
+        <button>Signup</button>
+    </form>
+
+    <?php
+
+    ?>
+
+</body>
+
+</html>
